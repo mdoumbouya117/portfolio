d18a04d33d249e550c37c714cfc818b5f5809f2e
mohamed-doumbouya <mohamed.doumbouya@externe.maif.fr>
2024-08-03 00:29:42 +0200

    assets & components ui

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,25 +1,27 @@
-import * as React from "react"
+import * as React from 'react'
+import { cn } from '@/lib/utils'
 
-import { cn } from "@/lib/utils"
-
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+interface InputProps extends React.ComponentPropsWithoutRef<'input'> {
+	className?: string
+	type?: string
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
+	({ className, type = 'text', ...props }, ref) => {
+		return (
+			<input
+				type={type}
+				className={cn(
+					'flex h-[48px] rounded-md border border-white/10 focus:border-accent font-light bg-primary px-4 py-5 text-base placeholder:text-white/60 outline-none',
+					className
+				)}
+				ref={ref}
+				{...props}
+			/>
+		)
+	}
 )
-Input.displayName = "Input"
+
+Input.displayName = 'Input'
 
 export { Input }
